eac70b2f4d53eab93e3ee98934c27eb13c2a6eb5
Scott Smerchek <ssmerche@gmail.com>
2012-07-20 13:28:09 -0700

    some tweaks

--- src/scrabble_solver/core.clj
+++ src/scrabble_solver/core.clj
@@ -3,13 +3,14 @@
 
 (defn supermultiset?
   [mset1 mset2]
-  (if (sets/superset? (set (keys mset1)) (set (keys mset2)))
-	(every? #(>= (mset1 (key %)) (mset2 (key %))) mset2)
-    false))
+  (let [letters1 (-> mset1 keys set) letters2 (-> mset2 keys set)]
+    (if (sets/superset? letters1 letters2)
+      (every? #(>= (mset1 (key %)) (mset2 (key %))) mset2)
+      false)))
 
 (def sort-by-length (partial sort #(compare (.length %2) (.length %))))
 
 (defn solve-with
   [words letters]
   (let [freqs (frequencies letters)]
-    (sort-by-length (filter #(supermultiset? freqs (frequencies %)) words))))
+    (->> words (filter #(supermultiset? freqs (frequencies %))) sort-by-length)))
--- src/scrabble_solver/main.clj
+++ src/scrabble_solver/main.clj
@@ -1,16 +1,17 @@
 (ns scrabble-solver.main 
-  (:require [scrabble-solver.core :as solver] [clojure.string :as string]))
+  (:require [scrabble-solver.core :as solver] [clojure.string :as string]
+            [clojure.java.io :as io]))
 
-(def massage-words (comp set (partial map string/lower-case)))
+(defn massage-words [words num-letters] 
+  (->> words (filter #(<= 2 (.length %) num-letters)) (map string/lower-case) set))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (with-open [rdr (clojure.java.io/reader "/usr/share/dict/words")]
-    (if (empty? args)
-      (println "ERROR: no letters given")
+  (if (empty? args)
+    (println "ERROR: no letters given")
+    (with-open [rdr (io/reader (or (second args) "/usr/share/dict/words"))]
       (let [letters (first args)
-            words (filter #(<=  2 (.length %) (.length letters)) 
-                          (massage-words (line-seq rdr)))]
+            words (massage-words (line-seq rdr) (.length letters)) ]
         (println (str "words for " letters))
         (prn (solver/solve-with words letters))))))
